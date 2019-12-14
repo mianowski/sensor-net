289d994e7f283b76fae571d21e604f8ab987ee83
gmianows <grzegorz.mianowski.ext@nokia.com>
2019-12-14 16:40:07 +0100

    Add boilerplate

---
+++ include/lib.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include<iostream>
+
+void foo()
+{
+    std::cout << "HI" << std::endl;
+}
---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(foo, should_pass) {
+  EXPECT_EQ(1, 1);
+}
